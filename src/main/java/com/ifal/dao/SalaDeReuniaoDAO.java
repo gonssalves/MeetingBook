b4f5f55c92cdbe3bc183ae1788b3df7a0d70d449
gonssalves <vgo1@ifal.edu.br>
2023-10-11 18:54:22 -0300

    style update

--- src/main/java/com/ifal/dao/SalaDeReuniaoDAO.java
+++ src/main/java/com/ifal/dao/SalaDeReuniaoDAO.java
@@ -15,13 +15,13 @@ public class SalaDeReuniaoDAO {
     }
 
     public void adicionarSala(SalaDeReuniaoBase sala) throws SQLException {
-        String sql = "INSERT INTO salas_de_reuniao (nome, capacidade, preco_hora, tipo) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO salas_de_reuniao (numero, capacidade, preco_hora, tipo) VALUES (?, ?, ?, ?)";
     
         try (PreparedStatement statement = conexao.prepareStatement(sql)) {
             statement.setInt(1, sala.getNumero());
             statement.setInt(2, sala.getCapacidade());
             statement.setDouble(3, sala.getPrecoHora());
-            statement.setString(4, sala.obterTipoSala()); // Método para obter o tipo da sala
+            statement.setString(4, sala.obterTipoSala());
     
             statement.executeUpdate();
         }
