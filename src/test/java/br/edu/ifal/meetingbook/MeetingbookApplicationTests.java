128b1b61d48de3cae883b402df70f1c9282cf43a
Vinícius Gonçalves de Oliveira <vgo1@aluno.ifal.edu.br>
2023-10-16 20:04:14 -0300

    create springboot project

---
+++ src/test/java/br/edu/ifal/meetingbook/MeetingbookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifal.meetingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
