e2b285bb5f6700491df108eea1350015db7e93bf
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-16 21:20:00 -0300

    feat: add post method

--- src/main/java/br/edu/ifal/meetingbook/user/UserController.java
+++ src/main/java/br/edu/ifal/meetingbook/user/UserController.java
@@ -1,5 +1,39 @@
 package br.edu.ifal.meetingbook.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    // Endpoint para criar um novo usuário
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        // Verifique se o usuário já existe no banco de dados.
+        var user = this.userRepository.findByEmail(userModel.getEmail());
+        
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        // Realize o hash da senha do usuário antes de armazená-lo no banco de dados.
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
+
+        // Salve o novo usuário no banco de dados.
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
 }
