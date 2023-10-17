85d400b8b302891d0f9861e9fa81b85d3b5b5dc9
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-16 21:08:04 -0300

    Update IUserRepository.java
    add methods to find a user

--- src/main/java/br/edu/ifal/meetingbook/user/IUserRepository.java
+++ src/main/java/br/edu/ifal/meetingbook/user/IUserRepository.java
@@ -1,5 +1,13 @@
 package br.edu.ifal.meetingbook.user;
 
-public class IUserRepository {
-    
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// A interface IUserRepository estende JpaRepository, que é um componente do Spring Data JPA
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    // Método para buscar um usuário por email
+    UserModel findByEmail(String email);
+
+    // Método para buscar um usuário por nome de usuário (username)
+    UserModel findByUsername(String username);
 }
