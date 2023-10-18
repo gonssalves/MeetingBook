dd6cf9b5cfb6c50b298ece168cfffb23ce9b6085
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-18 20:27:40 -0300

    feat: add column to identify room
    To column possesses an unique constraint.

--- src/main/java/br/edu/ifal/meetingbook/meetingroom/RoomModel.java
+++ src/main/java/br/edu/ifal/meetingbook/meetingroom/RoomModel.java
@@ -2,6 +2,7 @@ package br.edu.ifal.meetingbook.meetingroom;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,6 +16,8 @@ public abstract class RoomModel {
     @GeneratedValue(generator="UUID")
     private UUID id;
 
+    @Column(unique=true)
+    private int roomNumber;
     private int capacity;
     private float hourPrice;
     private String roomType;
