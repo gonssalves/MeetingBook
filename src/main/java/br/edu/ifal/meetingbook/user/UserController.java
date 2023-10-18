7265fc33329442dee1f5e98e4d17442a3b72d39e
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-17 23:00:58 -0300

    feat: add endpoint to update users

--- src/main/java/br/edu/ifal/meetingbook/user/UserController.java
+++ src/main/java/br/edu/ifal/meetingbook/user/UserController.java
@@ -1,6 +1,7 @@
 package br.edu.ifal.meetingbook.user;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.edu.ifal.meetingbook.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -61,6 +64,25 @@ public class UserController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity update(HttpServletRequest request, @RequestBody UserModel updatedUser) {
+        // Obtenha o ID do usuário autenticado a partir do atributo da requisição.
+        UUID authenticatedUserId = (UUID) request.getAttribute("idUser");
+
+        // Verifique se o usuário que está sendo atualizado existe.
+        Optional<UserModel> existingUser = userRepository.findById(authenticatedUserId);
+        if (!existingUser.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        // Realize a atualização dos campos permitidos.
+        UserModel userToUpdate = existingUser.get();
+        Utils.copyNonNullProperties(updatedUser, userToUpdate);
+        updatedUser = userRepository.save(userToUpdate);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
     }
 
     // Endpoint para excluir todos os usuários
