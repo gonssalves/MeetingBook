0f522552d4d0855ebdb84b4213ffda8de39cba85
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-18 17:56:08 -0300

    create room model

---
+++ src/main/java/br/edu/ifal/meetingbook/meetingroom/RoomModel.java
@@ -0,0 +1,28 @@
+package br.edu.ifal.meetingbook.meetingroom;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name="tbl_rooms")
+public abstract class RoomModel {
+    
+    @Id
+    @GeneratedValue(generator="UUID")
+    private UUID id;
+
+    private int capacity;
+    private float hourPrice;
+    private String roomType;
+
+    public abstract boolean hasVideoConference();
+
+    public abstract boolean hasCoffeMachine();
+
+    public abstract int numOfComputers();
+
+}
