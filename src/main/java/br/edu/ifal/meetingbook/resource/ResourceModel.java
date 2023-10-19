f56f8f28d12c01b3c1b7d538c5691089578b56b1
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-19 14:55:16 -0300

    feat: create resource model

---
+++ src/main/java/br/edu/ifal/meetingbook/resource/ResourceModel.java
@@ -0,0 +1,24 @@
+package br.edu.ifal.meetingbook.resource;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name="tbl_resources")
+public class ResourceModel {
+    
+    @Id
+    @GeneratedValue(generator="UUID")
+    private UUID id;
+
+    private int resourceNumber;
+    private String resourceName;
+    private String resourceType;
+
+    private UUID roomID;
+
+}
