e3b7adeabe3509e9a9120e0e15a51e777a8789a5
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-18 21:33:18 -0300

    feat: add endpoint to get only one room

--- src/main/java/br/edu/ifal/meetingbook/meetingroom/RoomController.java
+++ src/main/java/br/edu/ifal/meetingbook/meetingroom/RoomController.java
@@ -1,6 +1,7 @@
 package br.edu.ifal.meetingbook.meetingroom;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -38,6 +40,17 @@ public class RoomController {
     public List<RoomModel> listAll(HttpServletRequest request) {
         var rooms = this.roomRepository.findAll();
         return rooms;
+    }
+
+     @GetMapping("/{id}")
+    public ResponseEntity<Object> listOne(@PathVariable UUID id) {
+        var room = roomRepository.findById(id);
+
+        if (room == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sala não encontrada");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(room);
     }
     
 }
