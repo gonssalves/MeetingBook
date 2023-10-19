cc26e6addc94fff7b65f85fc93723993ede67bf9
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-19 14:55:56 -0300

    feat: create resource repository

---
+++ src/main/java/br/edu/ifal/meetingbook/resource/IResourceRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ifal.meetingbook.resource;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface IResourceRepository extends JpaRepository<ResourceModel, UUID> {
+    ResourceModel findByResourceNumber(int resourceNumber);
+    ResourceModel findByResourceName(String resourceName);
+    ResourceModel findByResourceType(String resourceType);
+    List<ResourceModel> findByRoomID(UUID roomID);
+}
