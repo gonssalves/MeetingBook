827978873cbd040af822126cc6549b8a5dfa7604
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-25 17:20:21 -0300

    FEAT: add booking get one endpoint

--- src/main/java/br/edu/ifal/meetingbook/booking/BookingController.java
+++ src/main/java/br/edu/ifal/meetingbook/booking/BookingController.java
@@ -1,11 +1,13 @@
 package br.edu.ifal.meetingbook.booking;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,6 +64,17 @@ public class BookingController {
     public List<BookingModel> listAll(HttpServletRequest request) {
         var bookings = this.bookingRepository.findAll();
         return bookings;
+    }    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> listOne(@PathVariable UUID id) {
+        var booking = bookingRepository.findById(id);
+        
+        if (booking == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Reserva não encontrada");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(booking);
     }    
 
     
