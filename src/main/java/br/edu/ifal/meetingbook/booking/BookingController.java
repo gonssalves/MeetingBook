3d2f8ca6a9ac73e3b6d6ab937bbdfcce267777a9
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-25 17:19:14 -0300

    FEAT: add booking get all endpoint

--- src/main/java/br/edu/ifal/meetingbook/booking/BookingController.java
+++ src/main/java/br/edu/ifal/meetingbook/booking/BookingController.java
@@ -1,15 +1,20 @@
 package br.edu.ifal.meetingbook.booking;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifal.meetingbook.meetingroom.IRoomRepository;
+import br.edu.ifal.meetingbook.resource.ResourceModel;
 import br.edu.ifal.meetingbook.user.IUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/bookings")
@@ -26,7 +31,7 @@ public class BookingController {
     
 
     @PutMapping("/")
-    public ResponseEntity create(@RequestBody BookingModel bookingModel) {
+    public ResponseEntity<Object> create(@RequestBody BookingModel bookingModel) {
         var booking = this.bookingRepository.findByBookingNumber(bookingModel.getBookingNumber());   
         
         if(booking == null) {
@@ -52,4 +57,12 @@ public class BookingController {
         var bookingCreated = this.bookingRepository.save(bookingModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(bookingCreated);
     }
+
+    @GetMapping("/")
+    public List<BookingModel> listAll(HttpServletRequest request) {
+        var bookings = this.bookingRepository.findAll();
+        return bookings;
+    }    
+
+    
 }
