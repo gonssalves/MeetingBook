3fc4bb42926a32ea1a1f5eca06b0a99640b828bd
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-25 15:33:40 -0300

    FEAT: add booking attributes

--- src/main/java/br/edu/ifal/meetingbook/booking/BookingModel.java
+++ src/main/java/br/edu/ifal/meetingbook/booking/BookingModel.java
@@ -1,5 +1,25 @@
 package br.edu.ifal.meetingbook.booking;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tbl_bookings")
 public class BookingModel {
     
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private String id;
+    
+    private String bookingDate;
+    private String bookingTime;
+    private float bookingPrice;
+    private String bookingStatus; // "Pendente", "Confirmada", "Cancelada"
+    
+    private UUID userId;
+    private UUID roomId;
 }
