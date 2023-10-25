1f60c6d2e8b857a7ebddff5e13d63800aed826f0
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-25 16:53:46 -0300

    FEAT: add findBy methods

--- src/main/java/br/edu/ifal/meetingbook/booking/IBookingRepository.java
+++ src/main/java/br/edu/ifal/meetingbook/booking/IBookingRepository.java
@@ -1,5 +1,16 @@
 package br.edu.ifal.meetingbook.booking;
 
-public class IBookingRepository {
-    
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IBookingRepository extends JpaRepository<BookingModel, UUID>{
+    List<BookingModel> findByUserId(UUID userId);
+    List<BookingModel> findByRoomId(UUID roomId);
+    List<BookingModel> findByBookingDate(String bookingDate);
+    List<BookingModel> findByBookingStatus(String bookingStatus);
+    List<BookingModel> findByUserAndRoomId(UUID userId, UUID roomId);
+    List<BookingModel> findByUserIdAndBookingStatus(UUID userId, String bookingStatus);
+    List<BookingModel> findByRoomIdAndBookingStatus(UUID roomId, String bookingStatus);   
 }
