09de3a69c88cdba8139ba7ecc343e700d7cbd508
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-10-27 18:51:57 -0300

    FIX: save instances on db when app start

--- src/main/java/br/edu/ifal/meetingbook/config/DataInitializer.java
+++ src/main/java/br/edu/ifal/meetingbook/config/DataInitializer.java
@@ -1,8 +1,5 @@
 package br.edu.ifal.meetingbook.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,7 +8,6 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.edu.ifal.meetingbook.booking.BookingModel;
 import br.edu.ifal.meetingbook.booking.IBookingRepository;
 import br.edu.ifal.meetingbook.meetingroom.IRoomRepository;
-import br.edu.ifal.meetingbook.meetingroom.RoomModel;
 import br.edu.ifal.meetingbook.meetingroom.concrete.LargeRoom;
 import br.edu.ifal.meetingbook.resource.IResourceRepository;
 import br.edu.ifal.meetingbook.resource.ResourceModel;
@@ -50,52 +46,50 @@ public class DataInitializer implements CommandLineRunner { // Permite executar 
         } if (roomRepository.count() == 0) {
             LargeRoom room = new LargeRoom(4, 13, 79.8f, "Large");
             roomRepository.save(room);
-
-        // } if (resourceRepository.count() == 0) {
-        //     RoomModel room = this.roomRepository.findByRoomNumber(4);
+            if (resourceRepository.count() == 0) {
+                ResourceModel resource = new ResourceModel();
+                resource.setResourceName("Computador");
+                resource.setResourceNumber(1);
+                resource.setResourceType("Eletrônico");
+                resource.setRoomID(room.getId());
             
-        //     ResourceModel resource = new ResourceModel();
-        //     resource.setResourceName("Computador");
-        //     resource.setResourceNumber(1);
-        //     resource.setResourceType("Eletrônico");
-        //     resource.setRoomID(room.getId());
+                // Salve o recurso no banco de dados
+                resourceRepository.save(resource);
 
-        //     resourceRepository.save(resource);
+                ResourceModel resource2 = new ResourceModel();
+                resource2.setResourceName("Máquina de Café");
+                resource2.setResourceNumber(5);
+                resource2.setResourceType("Eletrdoméstico");
+                resource2.setRoomID(room.getId());
+                
+                resourceRepository.save(resource2);
+                
+                ResourceModel resource3 = new ResourceModel();
+                resource3.setResourceName("Projetor");
+                resource3.setResourceNumber(3);
+                resource3.setResourceType("Eletrônico");
+                resource3.setRoomID(room.getId());
+                
+                resourceRepository.save(resource3);
 
-        //     resource = new ResourceModel();
-        //     resource.setResourceName("Máquina de Café");
-        //     resource.setResourceNumber(5);
-        //     resource.setResourceType("Eletrônico");
-        //     resource.setRoomID(room.getId());
-
-        //     resourceRepository.save(resource);
+            } if (bookingRepository.count() == 0) {
+                BookingModel booking = new BookingModel();
+                
+                booking.setBookingNumber(7);
+                booking.setBookingDate("25-10-2023");
+                booking.setBookingStartTime(13);
+                booking.setBookingEndTime(14);
+                booking.setBookingPrice(79.8f);
+                booking.setBookingStatus("Confirmado");
+                
+                UserModel user = this.userRepository.findByUsername("admin");
+    
+                booking.setRoomId(room.getId());
+                booking.setUserId(user.getId());
+    
+                bookingRepository.save(booking);
+            }
+            }
             
-        //     resource.setResourceName("Projetor");
-        //     resource.setResourceNumber(3);
-        //     resource.setResourceType("Eletrônico");
-        //     resource.setRoomID(room.getId());
-
-        //     resourceRepository.save(resource);
-
-        // } if (bookingRepository.count() == 0) {
-        //     BookingModel booking = new BookingModel();
-            
-        //     booking.setBookingNumber(7);
-        //     booking.setBookingDate("25-10-2023");
-        //     booking.setBookingStartTime(13);
-        //     booking.setBookingEndTime(14);
-        //     booking.setBookingPrice(79.8f);
-        //     booking.setBookingStatus("Confirmado");
-            
-        //     RoomModel room = this.roomRepository.findByRoomNumber(4);
-        //     UserModel user = this.userRepository.findByUsername("admin");
-
-        //     booking.setRoomId(room.getId());
-        //     booking.setUserId(user.getId());
-
-        //     bookingRepository.save(booking);
-        // }
-            
-}
-}
+    }
 }
