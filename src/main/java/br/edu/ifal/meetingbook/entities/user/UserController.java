359f033e5d834b5fc36959eaa3ece02c62d32523
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-11-05 14:32:20 -0300

    style: remove unused import

--- src/main/java/br/edu/ifal/meetingbook/entities/user/UserController.java
+++ src/main/java/br/edu/ifal/meetingbook/entities/user/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.ifal.meetingbook.entities.booking.BookingModel;
 import br.edu.ifal.meetingbook.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
