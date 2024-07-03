6b2260f2fa84ed15059964cecd9f1d9aeaac16cc
gonssalves <vgo1@aluno.ifal.edu.br>
2024-07-03 08:39:43 -0300

    chore: changes imports to accommodate junit5

--- src/test/java/br/edu/ifal/meetingbook/meetingroom/RoomModelUnitTest.java
+++ src/test/java/br/edu/ifal/meetingbook/meetingroom/RoomModelUnitTest.java
@@ -1,11 +1,13 @@
 package br.edu.ifal.meetingbook.meetingroom;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import br.edu.ifal.meetingbook.entities.meetingroom.concrete.LargeRoom;
 import br.edu.ifal.meetingbook.entities.meetingroom.concrete.MediumRoom;
 import br.edu.ifal.meetingbook.entities.meetingroom.concrete.SmallRoom;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class RoomModelUnitTest {
     
     @Test
--- src/test/java/br/edu/ifal/meetingbook/user/UserModelUnitTest.java
+++ src/test/java/br/edu/ifal/meetingbook/user/UserModelUnitTest.java
@@ -1,11 +1,13 @@
 package br.edu.ifal.meetingbook.user;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import br.edu.ifal.meetingbook.entities.user.UserModel;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserModelUnitTest {
     
     @Test
